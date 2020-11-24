99289c1ec4cc97d651bfc3a8b64c1149a8a1015e
thneves <tsneves11@gmail.com>
2020-11-24 19:35:10 -0300

    create Comments Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   validates :name, presence: true, uniqueness: true, length: { minimum: 3 }
   has_many :posts
+  has_many :comments
 end
---
+++ db/migrate/20201124221833_comment_migration.rb
@@ -0,0 +1,4 @@
+class CommentMigration < ActiveRecord::Migration[6.0]
+  def change
+  end
+end
