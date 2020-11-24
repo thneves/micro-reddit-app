6340c6d965da88902762e706394e8ef4107e71e6
janis-jenny <pcardenasvm@gmail.com>
2020-11-24 18:29:38 -0500

    add new Post model to try it in the console

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
   validates :title, presence: true, uniqueness: true
   validates :body, presence: true, length: { minumum: 10, maximum: 100 }
   belongs_to :user
+  has_many :comments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_24_220713) do
+ActiveRecord::Schema.define(version: 2020_11_24_221833) do
 
   create_table "comments", force: :cascade do |t|
     t.string "text"
