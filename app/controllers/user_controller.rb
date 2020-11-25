4f8dd58dc9f296792cd875541fbfdf4b12d06df4
thneves <tsneves11@gmail.com>
2020-11-25 13:00:45 -0300

    fix rubocop offenses

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,8 @@
+# rubocop:disable Style/EmptyMethod
+
 class UserController < ApplicationController
   def index
   end
 end
+
+# rubocop:enable Style/EmptyMethod
--- db/migrate/20201124221833_comment_migration.rb
+++ db/migrate/20201124221833_comment_migration.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class CommentMigration < ActiveRecord::Migration[6.0]
   def change; end
 end
